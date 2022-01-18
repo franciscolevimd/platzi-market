1847453257c61d66b2cd1a42e1184ef174384fc8
Francisco Leví <francisco.levi.md@gmail.com>
2022-01-18 12:44:22 -0600

    Hello world with Spting Boot.

---
+++ src/main/java/com/platzi/market/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.platzi.market;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String greet() {
+        return "I love cocaine!";
+    }
+}
