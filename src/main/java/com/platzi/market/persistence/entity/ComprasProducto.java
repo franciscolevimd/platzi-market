1dfa2db2a8c793a8f1b3629d0c4aa44a2f27e9f7
Francisco Leví <francisco.levi.md@gmail.com>
2022-01-18 18:39:02 -0600

    Composite keys.

---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProducto.java
@@ -0,0 +1,18 @@
+package com.platzi.market.persistence.entity;
+
+import lombok.Data;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
+@Data
+public class ComprasProducto {
+    @EmbeddedId
+    private ComprasProductoPK id;
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
+}
---
+++ src/main/java/com/platzi/market/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,16 @@
+package com.platzi.market.persistence.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+@Data
+public class ComprasProductoPK implements Serializable {
+    @Column(name = "id_compra")
+    private Integer idCompra;
+    @Column(name = "id_producto")
+    private Integer idProducto;
+}
