fc0c49750b9bc8b45112b0decfd6306e221426e2
Francisco Leví <francisco.levi.md@gmail.com>
2022-01-19 10:18:22 -0600

    Product Query Methos added.

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -4,11 +4,20 @@ import com.platzi.market.persistence.crud.ProductoCrudRespository;
 import com.platzi.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRespository productoCrudRespository;
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRespository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria)  {
+        return productoCrudRespository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoCrudRespository.finByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
