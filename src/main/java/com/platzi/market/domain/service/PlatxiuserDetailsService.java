c6b829c65ec695bb6d7ff11a9b655590723f5305
Francisco Leví <francisco.levi.md@gmail.com>
2022-01-25 16:16:40 -0600

    Authentication with spring security.

---
+++ src/main/java/com/platzi/market/domain/service/PlatxiuserDetailsService.java
@@ -0,0 +1,17 @@
+package com.platzi.market.domain.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class PlatxiuserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("levbono", "{noop}arisaurio123", new ArrayList<>());
+    }
+}
