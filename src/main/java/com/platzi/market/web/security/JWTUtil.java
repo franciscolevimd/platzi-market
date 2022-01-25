024f62e5c9da65923e5afd67fd2ef2bda6037f65
Francisco Leví <francisco.levi.md@gmail.com>
2022-01-25 16:55:08 -0600

    JWT authentication.

--- src/main/java/com/platzi/market/web/security/JWTUtil.java
+++ src/main/java/com/platzi/market/web/security/JWTUtil.java
@@ -3,12 +3,14 @@ package com.platzi.market.web.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
+@Component
 public class JWTUtil {
     private static final String KEY = "pl4tz1";
 
